32ec24a2c9a50997e96561481e16a1b9426e05eb
cdiagal <cdiagalprog@gmail.com>
2025-09-26 16:13:36 +0100

    se genera contenido

---
+++ ejercicio01/Ejercicio01.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Ejercicio01 - Operaciones con números naturales en PHP.
+ * 
+ * Enunciado
+ * Dado un fichero ops.csv con  columnas:
+ * z1,z2,op.
+ * Donde:
+ *  op ∈ {suma, resta, producto, division}
+ * Se debe calcular z1 (op) z2 y generar como salida un fichero resultado.csv.
+ */
+
+    
+ 
+
+
+
+?>
